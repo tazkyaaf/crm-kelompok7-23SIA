d11c783ac010d90638189cd797e462721d2df476
Ginta Sabil Ramadhani <ginta23si@mahasiswa.pcr.ac.id>
2025-05-26 14:53:26 +0700

    Menambah CustomerManagement by ginta

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./components/MainLayout";
+import CustomerManagement from "./pages/CustomerManagement";
 // import SalesManagement from "./pages/SalesManagement";
-// import CustomerManagement from "./pages/CustomerManagement";
+
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
-        {/* <Route path="penjualan" element={<SalesManagement />} />
-        <Route path="pelanggan" element={<CustomerManagement />} /> */}
+    
+        <Route path="pelanggan" element={<CustomerManagement />} />
       </Route>
     </Routes>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,6 +15,8 @@ const menuItems = [
   { name: 'Dashboard', icon: <LayoutDashboard />, path: '/' },
   { name: 'Produk', icon: <Box />, path: '/produk' },
   { name: 'Laporan', icon: <BarChart2 />, path: '/laporan' },
+  { name: 'Pelanggan', icon: <UserPlus />, path: '/pelanggan' },
+
 ]
 
 const accountItems = [
